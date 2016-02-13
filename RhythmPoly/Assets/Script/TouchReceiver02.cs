f9880391d6aa7b68bcc86e84cdd9bc46077fb1d0
SarahhhC <joytutu29@naver.com>
2016-02-14 00:50:53 +0900

    스코어 + 에러수정

--- RhythmPoly/Assets/Script/TouchReceiver02.cs
+++ RhythmPoly/Assets/Script/TouchReceiver02.cs
@@ -1,31 +1,90 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class TouchReceiver : MonoBehaviour
+public class TouchReceiver02 : MonoBehaviour
 {
-    public UILabel la;
+	public int count = 0;
+	public int score = 0;
+	public static bool IsAttached;
+	public static bool IsDetached;
+	PolygonSpawn ps;
+
+	public List<GameObject> scoreList;
+	public int finalScore;
+	bool isCorrectPoly = true;
 
     // Add Point
     public void TouchAttachPoint()
     {
-        Debug.Log("touch Attach");
-        la.text = "touch Attach";
+		count++;
+		DoubleTouchPoint();
+		IsAttached = true;
+		IsDetached = false;
+		for (int i = 0; i < 4 ; i++)
+			scoreList[i].SetActive (false);
     }
     // Sub Point
     public void TouchDetachPoint()
     {
-        Debug.Log("touch Detach");
-        la.text = "touch Detach";
+		count++;
+		DoubleTouchPoint();
+		IsAttached = false;
+		IsDetached = true;
+		for (int i = 0; i < 4 ; i++)
+			scoreList[i].SetActive (false);
     }
 
-    public void DoubleTouchPoint()
-    {
-        Debug.Log("Double Touch");
-        la.text = "Double Touch";
-    }
+	public void InitCount() 
+	{
+		count = 0;
+	}
+
+	public void DoubleTouchPoint() 
+	{
+		if (count == 2)
+		{
+			if (Input.anyKeyDown) {
+				if (isCorrectPoly) {
+					if (ps.GetFrontObject().transform.localPosition.z < -5) {
+						AllFalse ();
+						scoreList [0].SetActive (true);
+						finalScore += 0;
+					}
+					else if (ps.GetFrontObject().transform.localPosition.z < -3) {
+						AllFalse ();
+						scoreList [1].SetActive (true);
+						finalScore += 1;
+					}
+					else if (ps.GetFrontObject().transform.localPosition.z < -1) {
+						AllFalse ();
+						scoreList [2].SetActive (true);
+						finalScore += 2;
+					}
+					else if (ps.GetFrontObject().transform.localPosition.z < 0.005) {
+						AllFalse ();
+						scoreList [3].SetActive (true);
+						finalScore += 3;
+					}
+					else if (ps.GetFrontObject().transform.localPosition.z < 1) {
+						AllFalse ();
+						scoreList [2].SetActive (true);
+						finalScore += 2;
+					}
+				}
+			}
+			count = 0;
+			return;
+		}
+	}
+
+	void AllFalse() {
+		for (int i = 0; i < 4 ; i++)
+			scoreList[i].SetActive (false);
+	}
+
     public void TouchPause()
     {
         Debug.Log("touch Pause");
-        la.text = "touch Pause";
     }
 }
