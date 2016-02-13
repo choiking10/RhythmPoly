675eec3184880e97e0edcee59f48b2ab898711db
LukeKim <happychrono@gmail.com>
2016-02-14 02:27:56 +0900

    facebook 설정

---
+++ RhythmPoly/Assets/Script/Common/FacebookConnector.cs
@@ -0,0 +1,249 @@
+﻿using UnityEngine;
+using System.Collections;
+using Facebook.Unity;
+using System.Collections.Generic;
+using System;
+
+namespace RhythmPoly.Common
+{
+    public class FacebookConnector
+    {
+
+
+
+
+        FacebookdataParser fparser = new FacebookdataParser();
+        public FacebookConnector()
+        {
+
+        }
+
+
+        public void Init()
+        {
+            if(!FB.IsInitialized)
+                FB.Init(this.OnInitComplete, this.OnHideUnity);
+
+        }
+
+
+        private void OnInitComplete()
+        {
+
+        }
+
+
+
+        public void GetMyInfo()
+        {
+            string ApiQuery = "me/?fields=id,name,picture,score";
+            FB.API(ApiQuery, HttpMethod.GET, CallbackMyInfo);
+        }
+
+        public void CallbackMyInfo(IGraphResult result)
+        {
+            if (!string.IsNullOrEmpty(result.RawResult))
+            {
+                fparser.SetMyInfo(result.RawResult);
+            }
+        }
+
+
+
+
+
+        private void OnHideUnity(bool isGameShown)
+        {
+        }
+
+        public bool IsLoggedIn()
+        {
+            return FB.IsLoggedIn;
+        }
+
+        public void Login()
+        {
+            #if UNITY_ANDROID  && !UNITY_EDITOR
+                CallFBLogin();
+            #endif
+
+        }
+        private void CallFBLogin()
+        {
+            FB.LogInWithReadPermissions(new List<string>() { "public_profile", "email", "user_friends" }, this.LoginHandleResult);
+        }
+
+
+
+            
+
+        string LastResponse = "";
+        string Status;
+        protected void LoginHandleResult(IResult result)
+        {
+            if (result == null)
+            {
+                this.LastResponse = "Null Response\n";
+                return;
+            }
+
+
+            // Some platforms return the empty string instead of null.
+            if (!string.IsNullOrEmpty(result.Error))
+            {
+                this.Status = "Error - Check log for details";
+            }
+            else if (result.Cancelled)
+            {
+                this.Status = "Cancelled - Check log for details";
+                this.LastResponse = "Cancelled Response:\n" + result.RawResult;
+            }
+            else if (!string.IsNullOrEmpty(result.RawResult))
+            {
+                this.Status = "Success - Check log for details";
+                this.LastResponse = "Success Response:\n" + result.RawResult;
+                
+            }
+            else
+            {
+                this.LastResponse = "Empty Response\n";
+            }
+        }
+
+
+        public void GetFriendsList()
+        {
+            string ApiQuery = "me/friends?fields=name,id,picture,score";
+            FB.API(ApiQuery, HttpMethod.GET, CallbackFriends);
+        }
+
+        public void CallbackFriends(IGraphResult result)
+        {
+            if (!string.IsNullOrEmpty(result.RawResult))
+            {
+                fparser.SetFriendList(result.RawResult);
+
+                List<FriendInfo> list = fparser.GetFriendList();
+                for (int i = 0; i < list.Count; ++i)
+                {
+                    this.LastResponse = list[i].name.ToString() + " : " + list[i].score.ToString() + " : ";
+                }
+
+            }
+
+        }
+
+
+
+
+        protected void PublishHandleResult(IResult result)
+        {
+            if (result == null)
+            {
+                this.LastResponse = "Null Response\n";
+                return;
+            }
+
+
+            // Some platforms return the empty string instead of null.
+            if (!string.IsNullOrEmpty(result.Error))
+            {
+                this.Status = "Error - Check log for details";
+            }
+            else if (result.Cancelled)
+            {
+                this.Status = "Cancelled - Check log for details";
+                this.LastResponse = "Cancelled Response:\n" + result.RawResult;
+            }
+            else if (!string.IsNullOrEmpty(result.RawResult))
+            {
+                this.Status = "Success - Check log for details";
+                this.LastResponse = "Success Response:\n" + result.RawResult;
+            }
+            else
+            {
+                this.LastResponse = "Empty Response\n";
+            }
+        }
+
+        public void CallFBLoginForPublish()
+        {
+            if(!FB.IsInitialized)
+            {
+                Init();
+                return;
+            }
+
+            if (!FB.IsLoggedIn)
+            {
+                FB.LogInWithReadPermissions(new List<string>() { "public_profile", "email", "user_friends" }, delegate (ILoginResult result)
+                {
+                    if (!string.IsNullOrEmpty(result.RawResult))
+                    {
+                        //success
+                        FB.LogInWithPublishPermissions(new List<string>() { "publish_actions" }, this.PublishHandleResult);
+                    }
+                });
+            }
+            else
+            {
+                // It is generally good behavior to split asking for read and publish
+                // permissions rather than ask for them all at once.
+                //
+                // In your own game, consider postponing this call until the moment
+                // you actually need it.
+                FB.LogInWithPublishPermissions(new List<string>() { "publish_actions" }, this.PublishHandleResult);
+            }
+        }
+
+
+        public void LogOut()
+        {
+            FB.LogOut();
+        }
+
+
+
+        public void SetScore(long score)
+        {
+            if (!FB.IsInitialized || !FB.IsLoggedIn)
+                return;
+            var scoreData = new Dictionary<string, string>();
+            string tmpscore = score.ToString();
+            scoreData["score"] = tmpscore;
+            FB.API("/me/scores", HttpMethod.POST, delegate (IGraphResult result)
+            {
+                if (!string.IsNullOrEmpty(result.RawResult))
+                {
+                    //success
+                }
+            }, scoreData);
+        }
+       
+
+      
+
+        public IEnumerator TakeScreenshot()
+        {
+            yield return new WaitForEndOfFrame();
+
+            var width = Screen.width;
+            var height = Screen.height;
+            var tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+            // Read screen contents into the texture
+            tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+            tex.Apply();
+            byte[] screenshot = tex.EncodeToPNG();
+
+            var wwwForm = new WWWForm();
+            wwwForm.AddBinaryData("image", screenshot, "InteractiveConsole.png");
+
+            FB.API("me/photos", HttpMethod.POST, Callback, wwwForm);
+        }
+
+        public void Callback(IGraphResult result)
+        {
+
+        }
+    }
+}
---
+++ RhythmPoly/Assets/Script/Common/FacebookdataParser.cs
@@ -0,0 +1,271 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Facebook.MiniJSON;
+namespace RhythmPoly.Common
+{
+    public struct FriendInfo
+    {
+        public string id;
+        public string name;
+        public bool hasPicture;
+        public string pictureUrl;
+        public long score;
+        public FriendInfo(string id, string name, bool hasPicture, string pictureUrl, long score)
+        {
+            this.id = id;
+            this.name = name;
+            this.hasPicture = hasPicture;
+            this.pictureUrl = pictureUrl;
+            this.score = score;
+        }
+    }
+
+    public struct FriendScore
+    {
+        string id;
+        string score;
+        public FriendScore(string id, string score)
+        {
+            this.id = id;
+            this.score = score;
+        }
+    }
+
+
+    public class FacebookdataParser
+    {
+        List<FriendInfo> friendInfoList = new List<FriendInfo>();
+        List<FriendScore> friendScoreList = new List<FriendScore>();
+
+        FriendInfo myInfo = new FriendInfo();
+
+        public FacebookdataParser()
+        {
+
+        }
+
+        public List<FriendInfo> GetFriendList()
+        {
+            return friendInfoList;
+        }
+
+        public List<FriendScore> GetScoreList()
+        {
+            return friendScoreList;
+        }
+
+
+        public void SetMyInfo(string jsonData)
+        {
+            myInfo = new FriendInfo();
+            try
+            {
+                Dictionary<string, object> dict = (Dictionary<string, object>)Json.Deserialize(jsonData);
+
+
+                Dictionary<string, object> tempdic = dict as Dictionary<string, object>;
+                string id = "";
+                string name = "";
+                bool hasPicture = false;
+                string url = "";
+                long score = 0L;
+                object temp;
+
+                if (tempdic.TryGetValue("id", out temp))
+                {
+                    id = tempdic["id"] as string;
+                }
+                if (tempdic.TryGetValue("name", out temp))
+                {
+                    name = tempdic["name"] as string;
+                }
+                if (tempdic.TryGetValue("picture", out temp))
+                {
+
+                    Dictionary<string, object> tempdic2 = tempdic["picture"] as Dictionary<string, object>;
+                    var tempdic3 = tempdic2["data"] as Dictionary<string, object>;
+                    object siluet = tempdic3["is_silhouette"];
+                    if (siluet is bool)
+                    {
+                        hasPicture = !(bool)(siluet);
+                    }
+                    url = tempdic3["url"] as string;
+                }
+
+                if (tempdic.TryGetValue("score", out temp))
+                {
+
+                    Dictionary<string, object> tempdic2 = tempdic["score"] as Dictionary<string, object>;
+                    List<object> scoreslist = (List<object>)tempdic2["data"];
+                    for (int j = 0; j < scoreslist.Count; ++j)
+                    {
+                        Dictionary<string, object> tempdicScore = scoreslist[j] as Dictionary<string, object>;
+
+                        if (tempdicScore.TryGetValue("score", out temp))
+                        {
+                            var tempobj = tempdicScore["score"] as object;
+                            if (tempobj is long)
+                            {
+                                score = (long)tempobj;
+                            }
+                        }
+
+                        /*
+                        if (tempdicScore.TryGetValue("user", out temp))
+                        {
+                            Dictionary<string, object> tempdicUser = tempdic5["user"] as Dictionary<string, object>;
+                            uid = tempdicUser["id"] as string;
+                        }
+                        */
+
+                    }
+
+
+                }
+
+                myInfo = new FriendInfo(id, name, hasPicture, url, score);
+
+            }
+            catch
+            {
+
+            }
+        }
+
+
+    public void SetFriendList(string jsonData)
+        {
+
+            friendInfoList.Clear();
+            friendScoreList.Clear();
+            try
+            {
+                Dictionary<string, object> dict = (Dictionary<string, object>)Json.Deserialize(jsonData);
+
+                object temp;
+                List<object> friends = new List<object>();
+                if (dict.TryGetValue("data", out temp))
+                {
+                    friends = (List<object>)dict["data"];
+                    if (friends.Count < 1)
+                        return;
+
+                    for (int i = 0; i < friends.Count; ++i)
+                    {
+                        Dictionary<string, object> tempdic = friends[i] as Dictionary<string, object>;
+                        string id = "";
+                        string name = "";
+                        bool hasPicture = false;
+                        string url = "";
+                        long score = 0L;
+
+                        if (tempdic.TryGetValue("id", out temp))
+                        {
+                            id = tempdic["id"] as string;
+                        }
+                        if (tempdic.TryGetValue("name", out temp))
+                        {
+                            name = tempdic["name"] as string;
+                        }
+                        if (tempdic.TryGetValue("picture", out temp))
+                        {
+
+                            Dictionary<string, object> tempdic2 = tempdic["picture"] as Dictionary<string, object>;
+                            var tempdic3 = tempdic2["data"] as Dictionary<string, object>;
+                            object siluet = tempdic3["is_silhouette"];
+                            if (siluet is bool)
+                            {
+                                hasPicture = !(bool)(siluet);
+                            }
+                            url = tempdic3["url"] as string;
+                        }
+
+                        if (tempdic.TryGetValue("score", out temp))
+                        {
+
+                            Dictionary<string, object> tempdic2 = tempdic["score"] as Dictionary<string, object>;
+                            List<object> scoreslist = (List<object>)tempdic2["data"];
+                            for (int j = 0; j < scoreslist.Count; ++j)
+                            {
+                                Dictionary<string, object> tempdicScore = scoreslist[j] as Dictionary<string, object>;
+                              
+                                if (tempdicScore.TryGetValue("score", out temp))
+                                {
+                                    var tempobj = tempdicScore["score"] as object;
+                                    if(tempobj is long)
+                                    {
+                                        score = (long)tempobj;
+                                    }
+                                }
+
+                                /*
+                                if (tempdicScore.TryGetValue("user", out temp))
+                                {
+                                    Dictionary<string, object> tempdicUser = tempdic5["user"] as Dictionary<string, object>;
+                                    uid = tempdicUser["id"] as string;
+                                }
+                                */
+
+                            }
+
+
+                        }
+
+                        friendInfoList.Add(new FriendInfo(id, name, hasPicture, url, score));
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        /*app/scores?fields/score,user.limit(30)*/
+        public void SetScoreList(string jsonData)
+        {
+            friendScoreList.Clear();
+            try
+            {
+                Dictionary<string, object> dict = (Dictionary<string, object>)Json.Deserialize(jsonData);
+                object temp;
+                List<object> scores = new List<object>();
+                if (dict.TryGetValue("data", out temp))
+                {
+                    scores = (List<object>)dict["data"];
+                    if (scores.Count < 1)
+                        return;
+
+                    for (int i = 0; i < scores.Count; ++i)
+                    {
+
+                        Dictionary<string, object> tempdic = scores[i] as Dictionary<string, object>;
+                        string score = "";
+                        string id = "";
+                        if (tempdic.TryGetValue("score", out temp))
+                        {
+                            score = tempdic["score"] as string;
+                        }
+
+                        if (tempdic.TryGetValue("user", out temp))
+                        {
+
+                            Dictionary<string, object> tempdicsub = tempdic["user"] as Dictionary<string, object>;
+                            id = tempdicsub["id"] as string;
+                        }
+
+                        friendScoreList.Add(new FriendScore(id, score));
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+
+
+    }
+}
