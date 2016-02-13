d0064222791d025f27b6f1ad72c6310017348e64
LukeKim <happychrono@gmail.com>
2016-02-13 22:48:04 +0900

    [Common] 유저 정보 저장 [Assets] ZPlayerPrefs

---
+++ RhythmPoly/Assets/Script/ServerTest/MainCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using RhythmPoly.Common;
+public class MainCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //ZPlayerPrefs.Initialize("UnitoneGames", "unitonepwpw75");
+        //ZPlayerPrefs.SetString("score", "5");
+
+        //Debug.Log(ZPlayerPrefs.GetString("score
+        UserInfo.Instance.Score = 10;
+        Debug.Log(UserInfo.Instance.Score); 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
