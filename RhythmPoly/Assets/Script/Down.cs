c7f122e47eadafefc5a93d4e4499fcb97b8f1d9d
최윤호 <choiking10@naver.com>
2016-02-13 15:59:39 +0900

    Add Touch Receiver

--- RhythmPoly/Assets/Script/Down.cs
+++ RhythmPoly/Assets/Script/Down.cs
@@ -21,15 +21,4 @@ public class Down : MonoBehaviour {
             gameObject.transform.eulerAngles += new Vector3(0, speed * angspeed, 0);
         }
 	}
-    void OnPostRender()
-    {
-        GL.PushMatrix();
-        GL.LoadOrtho();
-        GL.Begin(GL.LINES);
-        GL.Color(Color.red);
-        GL.Vertex(startVertex);
-        GL.Vertex(new Vector3(mousePos.x / Screen.width, mousePos.y / Screen.height, 0));
-        GL.End();
-        GL.PopMatrix();
-    }
 }
---
+++ RhythmPoly/Assets/Script/TouchReceiver.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchReceiver : MonoBehaviour {
+
+    void TouchAttachPoint()
+    {
+        Debug.Log("touch Attach");
+    }
+    void TouchDetachPoint()
+    {
+        Debug.Log("touch Detach");
+    }
+    void TouchPause()
+    {
+        Debug.Log("touch Puase");
+    }
+}
