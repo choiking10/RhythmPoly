40f5e563fad59b5d9daf81b18af414e2c3aca6a3
최윤호 <choiking10@naver.com>
2016-04-16 16:08:16 +0900

    광고, 씬연결 수정 완료

---
+++ RhythmPoly/Assets/Script/UI/EndSceneButtonListener.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndSceneButtonListener : MonoBehaviour 
+{
+    public void Retry()
+    {
+        Application.LoadLevel("InGameScene");
+        Debug.Log("hello");
+    }
+    public void GoogleRanking()
+    {
+
+    }
+    
+}
---
+++ RhythmPoly/Assets/Script/UI/EndSceneSetScore.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EndSceneSetScore : MonoBehaviour 
+{
+    public void Start(){
+        Text t = gameObject.GetComponent<Text>();
+        t.text = ""+TouchReceiver02.finalScore;
+    }
+}
+
